f12bb6bd6797957badd4670f4cc2ae15aca65892
hankakee <juniorhankakee@gmail.com>
2022-11-30 21:06:26 -0800

    Fix bug 'persisted...' in list view carousel

--- lib/widget/cardslider.dart
+++ lib/widget/cardslider.dart
@@ -1,29 +1,51 @@
 import 'package:efundusv2/widget/card_course.dart';
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
-class CardCourseFactorySlider extends StatelessWidget {
+// class CardCourseFactorySlider extends StatelessWidget {
+//   final int qty;
+//   final String namecourse = "";
+//   const CardCourseFactorySlider({Key? key, required this.qty})
+//       : super(key: key);
+
+class CardCourseFactorySlider extends StatefulWidget {
   final int qty;
-  const CardCourseFactorySlider({Key? key, required this.qty})
-      : super(key: key);
+  final String namecourse = "";
+  const CardCourseFactorySlider({Key? key, required this.qty});
 
+  @override
+  State<CardCourseFactorySlider> createState() =>
+      _CardCourseFactorySliderState();
+}
+
+class _CardCourseFactorySliderState extends State<CardCourseFactorySlider> {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 300.0,
-      child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemCount: qty,
-          itemBuilder: (BuildContext context, int index) {
-            return Container(
-              margin: const EdgeInsets.symmetric(horizontal: 1.0),
-              child: const CardCourse(
-                isFullScreen: false,
-                named: "Lbl named",
-                hasDescribe: false,
-                hasleftShopppingIcon: true,
-              ),
-            );
-          }),
-    );
+        height: 302.0,
+        child: CarouselSlider(
+            options: CarouselOptions(
+                height: 300.0,
+                reverse: false,
+                autoPlay: false,
+                enableInfiniteScroll: false,
+                // fulscreen carrousel
+                padEnds: false,
+                initialPage: 0),
+            items: [1, 2, 3, 4, 5].map((i) {
+              return Builder(
+                builder: (BuildContext context) {
+                  return Container(
+                    width: MediaQuery.of(context).size.width,
+                    child: CardCourse(
+                      isFullScreen: false,
+                      named: widget.namecourse,
+                      hasDescribe: false,
+                      hasleftShopppingIcon: true,
+                    ),
+                  );
+                },
+              );
+            }).toList()));
   }
 }
